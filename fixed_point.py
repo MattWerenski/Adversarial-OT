dd278fdc9cf0785295c28fe3afacf581a7ad07a6
Matthew Werenski <MattWerenski@github.com>
2023-04-03 22:24:26 -0400

    Updated fixed point approach code

--- fixed_point.py
+++ fixed_point.py
@@ -6,6 +6,48 @@ from scipy.spatial.distance import cdist
 from scipy.spatial.distance import euclidean
 from scipy.stats import dirichlet
 
+def prune(Xs, threshold, distance):
+    """
+    prune removes all points that are more than the threshold away from every
+    point in every other color
+    
+    :param: list of datasets of points
+    :param threshold: location of the hard threshold
+    :param distance: distance function to compute pairwise distances
+    :return: two datasets, containing non-isolated and isolated points resp.
+    """
+    
+    close_Xs = []
+    far_Xs = []
+    
+    # iterate over each class splitting the datasets
+    for i in range(len(Xs)):
+        Xi = Xs[i]
+        
+        # has_close[i] will be 0 if it has no nearby points, pos. o.w.
+        has_close = np.zeros(Xi.shape[0])
+        
+        # compare against each other class
+        for j in range(len(Xs)):
+            if i == j: 
+                continue
+            
+            # computes thresholded cost
+            cost = cdist(Xi,Xs[j], metric=distance) <= threshold*2
+            
+            # and adds the indicators 
+            has_close += np.sum(cost, axis=1)
+            
+        # convert to True-False
+        has_close = has_close > 0
+        close_inds = np.where(has_close)[0]
+        far_inds = np.where(1-has_close)[0]
+        
+        close_Xs += [Xi[close_inds]]
+        far_Xs += [Xi[far_inds]]
+        
+    return close_Xs, far_Xs
+
 
 def generate_cost(X, Y, threshold, distance, barrier=1):
     """
@@ -24,30 +66,82 @@ def generate_cost(X, Y, threshold, distance, barrier=1):
     return cost
 
 
-def solve_partial_ots(Xs, Y, Ymass, threshold, distance, barrier=1):
+def generate_cost_move_far(X, Y, threshold, distance, barrier=1, slope=1):
+    """
+    generate_cost given a pair of datapoints, gives a soft approximation to
+    the indicator cost.
+    
+    :param X: first dataset of points
+    :param Y: second dataset of points
+    :param threshold: location of the soft threshold
+    :param distance: distance function to compute pairwise distances
+    :param barrier: parameter determining how hard the threshold should be
+    :param slope: slope to use to encourage moving far away
+    :return: cost matrix approximating the indicator 
+    """
+    base_cost = cdist(X,Y, metric=distance)
+    cost = (np.exp(barrier *(base_cost - threshold)) + 5 * threshold) * (base_cost > threshold) \
+        - slope*base_cost * (base_cost <= threshold)
+    return cost
+
+def solve_partial_ots(Xs, Xmasses, Y, Ymass, threshold, distance, barrier=1, slope=0):
     """
     solve_partial_ots given a set of source distributions and a shared target
     computes the partial transport from each source to the target
     
     :param Xs: list of dataset of points
+    :param Xmasses: amount of mass at each point. Xmasses[i] is a ~scalar~
     :param Y: second dataset of points
     :param Ymass: amount of mass at each point in Y
     :param threshold: location of the soft threshold
     :param distance: distance function to compute pairwise distances
     :param barrier: parameter determining how hard the threshold should be
+    :param slope: slope for encouraging mass to move far, if this is 0
+        then the soft barrier cost is used
     :return: list of partial couplings
     """
     
     gammas = []
-    for X in Xs:
-        Xmass = np.ones(X.shape[0]) / X.shape[0]
-        cost = generate_cost(X, Y, threshold, distance, barrier=barrier)
-        gammas += [ot.partial.partial_wasserstein(Xmass, Ymass, cost, m=1, nb_dummies=10)]
+    for X, mass in zip(Xs, Xmasses):
+        Xmass = np.ones(X.shape[0]) * mass
+        if slope == 0:
+            cost = generate_cost(X, Y, threshold, distance, barrier=barrier)
+        else:
+            cost = generate_cost_move_far(X, Y, threshold, distance, barrier=barrier, slope=slope)
+        gammas += [ot.partial.partial_wasserstein(Xmass, Ymass, cost, m=np.sum(Xmass), nb_dummies=10)]
         
     return gammas
 
+def solve_partial_ot(X, Xmass, Y, Ymass, threshold, distance, Y_uses, barrier=1, slope=0, reward=0):
+    """
+    solve_partial_ots given a set of source distributions and a shared target
+    computes the partial transport from each source to the target
+    
+    :param X: dataset of points
+    :param Xmass
+    :param Y: second dataset of points
+    :param Ymass: amount of mass at each point in Y
+    :param threshold: location of the soft threshold
+    :param distance: distance function to compute pairwise distances
+    :param barrier: parameter determining how hard the threshold should be
+    :param slope: slope for encouraging mass to move far, if this is 0
+        then the soft barrier cost is used
+    :return: list of partial couplings
+    """
+    
+    Xmass = np.ones(X.shape[0]) * Xmass
+    if slope == 0:
+        cost = generate_cost(X, Y, threshold, distance, barrier=barrier)
+    else:
+        cost = generate_cost_move_far(X, Y, threshold, distance, barrier=barrier, slope=slope)
+    cost -= reward * Y_uses.sum(1) # don't add a reward for points beyond threshold.
+    
+    gamma = ot.partial.partial_wasserstein(Xmass, Ymass, cost, m=np.sum(Xmass), nb_dummies=10)
+        
+    return gamma
 
-def update_Y_locations(Xs, Y, gammas):
+
+def update_Y_locations(Xs, Y, gammas, Y_uses=None, interpolation=0.1):
     """
     update_Y_locations moves the locations of the points in Y to the 
     centroids of the mass being assigned to them
@@ -55,6 +149,8 @@ def update_Y_locations(Xs, Y, gammas):
     :param Xs: list of dataset points
     :param Y: second dataset of points to be updated
     :param gammas: partial couplings
+    :param Y_uses: 2D boolean array for if Y_uses[i,j] is if the i'th Y point
+        is used by the j'th color.
     :return: updated locations as well as the updated mass assignments
     """
     
@@ -73,8 +169,12 @@ def update_Y_locations(Xs, Y, gammas):
     dominating_mass = np.max(np.sum(gammas, axis=1), axis=0)
     
     # removes unused points 
-    centroids = centroids[total_assignment > 0]
+    centroids = (interpolation*centroids + (1-interpolation)*Y)[total_assignment > 0]
     dominating_mass = dominating_mass[total_assignment > 0]
+    
+    if not Y_uses is None:
+        new_uses = Y_uses[total_assignment > 0]
+        return centroids, dominating_mass, new_uses
     
     return centroids, dominating_mass
 
@@ -104,7 +204,7 @@ def generate_initial_Y(Xs, npoints=40):
     
     return Y
 
-def fixed_point(Xs, threshold, distance, npoints=50, num_iters=25, barrier=1):
+def fixed_point(Xs, threshold, distance, npoints=50, num_iters=25, barrier=1, slope=0):
     """
     fixed_point given a set of measures, runs a fixed point approach to find
     the adversarial measure
@@ -115,23 +215,128 @@ def fixed_point(Xs, threshold, distance, npoints=50, num_iters=25, barrier=1):
     :param npoint: number of support points to start the adversary with
     :param num_iters: number of fixed point iterations to run for
     :param barrier: parameter determining how hard the threshold should be
-    :return: the support, mass, and assignments for the dominating measure
+    :param slope: slope for encouraging mass to move far, if this is 0
+        then the soft barrier cost is used
+    :return: the support, mass, assignments for the dominating measure
+        and re-ordered Xs
     """
+    
+    Xmasses = [1 / len(X) for X in Xs]
+    close_Xs, far_Xs = prune(Xs, threshold, distance)
     
     # generates the initial setting of Y measure
     K = len(Xs)
-    Y = generate_initial_Y(Xs, npoints=npoints)
-    Ymass = np.ones(npoints) / npoints * K 
+    Y = generate_initial_Y(close_Xs, npoints=npoints)
+    Ymass = np.ones(npoints) 
     
     # applies the fixed point updates 
     for i in range(num_iters):
         # solve the partial optimal transports
-        gammas = solve_partial_ots(Xs, Y, Ymass, threshold, distance, barrier=barrier)
+        gammas = solve_partial_ots(close_Xs, Xmasses, Y, Ymass, threshold, distance, \
+                                   barrier=barrier, slope=slope)
+        
         # and uses those to update the measure
-        Y,Ymass = update_Y_locations(Xs, Y, gammas)
+        Y,Ymass = update_Y_locations(close_Xs, Y, gammas)
+        
+    # rebuilds the Ys with the pruned support added back in
+    Y = np.concatenate([Y]+far_Xs)
+    Ymass = np.concatenate([Ymass]+[np.ones(len(cX))*m for cX,m in zip(far_Xs, Xmasses)])
+    
+    # rebuilds the gammas with the mass put back in
+    full_gammas = []
+    npoints = np.sum([len(X) for X in Xs])
+    start_index = 1
+    for k in range(K):
+        
+        fg = np.zeros((len(Xs[k]), len(Ymass)))
+        # copy over the original gamma
+        fg[0:gammas[k].shape[0], 0:gammas[k].shape[1]] = gammas[k]
+        
+        # add in the extra part for far points
+        start = gammas[k].shape[1]+start_index-1
+        end = start+len(far_Xs[k])
+        start_index += len(far_Xs[k])
+        
+        fg[len(close_Xs[k]):, start:end] = np.eye(end-start) / len(Xs[k])
+        
+        full_gammas += [fg]
+        
+    # rebuilds the support of the Xs
+    rebuild_Xs = [np.concatenate([close_X, far_X]) for close_X, far_X in zip(close_Xs, far_Xs)]
     
     # the last gammas are useful for figuring out what the source is being moved to.
-    return Y, Ymass, gammas
+    return Y, Ymass, full_gammas, rebuild_Xs
+
+def incentive_fixed_point(Xs, threshold, distance, npoints, num_iters=100, 
+                          barrier=1, slope=0, reward=-1):
+    """
+    incentive_fixed_point given a set of measures, runs a fixed point approach with 
+    encouragement for using points that are being used by other classes
+    :param Xs: list of lists of datapoints, one for each class 
+    :param threshold: location of the soft threshold
+    :param distance: distance function to compute pairwise distances
+    :param npoint: number of support points to start the adversary with
+    :param num_iters: number of fixed point iterations to run for
+    :param barrier: parameter determining how hard the threshold should be
+    :param slope: slope for encouraging mass to move far, if this is 0
+        then the soft barrier cost is used
+    :param reward: amount to reduce the cost by if the point is being used
+    :return: the support, mass, assignments for the dominating measure
+        and re-ordered Xs
+    """
+    
+    Xmasses = [1/len(X) for X in Xs]
+    close_Xs, far_Xs = prune(Xs, threshold, distance)
+    
+    # generates the initial setting of Y measure
+    K = len(Xs)
+    Y = generate_initial_Y(close_Xs, npoints=npoints)
+    Ymass = np.ones(npoints) 
+    Y_uses = np.zeros((npoints, K))
+    
+    # applies the fixed point updates 
+    for i in range(num_iters):
+        
+        gammas = []
+        for j in range(K):
+            
+            Y_uses[:,j] = 0
+            gamma = solve_partial_ot(close_Xs[j], Xmasses[j], Y, Ymass, threshold, distance, Y_uses, \
+                                     barrier=barrier, slope=slope, reward=reward)
+            Y_uses[:,j] = gamma.sum(0) > 0
+            gammas += [gamma]
+        
+        # and uses those to update the measure
+        Y, Ymass, Y_uses = update_Y_locations(close_Xs, Y, gammas, Y_uses=Y_uses)
+    
+    # rebuilds the Ys with the pruned support added back in
+    Y = np.concatenate([Y]+far_Xs)
+    Ymass = np.concatenate([Ymass]+[np.ones(len(cX))*m for cX,m in zip(far_Xs, Xmasses)])
+    
+    # rebuilds the gammas with the mass put back in
+    full_gammas = []
+    npoints = np.sum([len(X) for X in Xs])
+    start_index = 1
+    for k in range(K):
+        
+        fg = np.zeros((len(Xs[k]), len(Ymass)))
+        # copy over the original gamma
+        fg[0:gammas[k].shape[0], 0:gammas[k].shape[1]] = gammas[k]
+        
+        # add in the extra part for far points
+        start = gammas[k].shape[1]+start_index-1
+        end = start+len(far_Xs[k])
+        start_index += len(far_Xs[k])
+        
+        fg[len(close_Xs[k]):, start:end] = np.eye(end-start) / len(Xs[k])
+        
+        full_gammas += [fg]
+        
+    # rebuilds the support of the Xs
+    rebuild_Xs = [np.concatenate([close_X, far_X]) for close_X, far_X in zip(close_Xs, far_Xs)]
+    
+    # the last gammas are useful for figuring out what the source is being moved to.
+    return Y, Ymass, full_gammas, rebuild_Xs
 
 def violating_mass(Xs, Y, gammas, threshold, distance):
     """
--- tests/baseline_comparison_test.py
+++ tests/baseline_comparison_test.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.spatial.distance import euclidean
+from scipy.stats import multivariate_normal
 from cvxopt import solvers
 import sys
 import os
@@ -8,7 +9,7 @@ sys.path.append(os.path.abspath('../'))
 from colored_rips import basic_rips, colored_rips
 from entropic import approximate_MOT
 from indicator_solver import solve
-from fixed_point import fixed_point, violating_mass
+from fixed_point import incentive_fixed_point, violating_mass
 
 solvers.options['show_progress'] = False
 
@@ -17,14 +18,33 @@ n = 12
 linspace = np.arange(n)
 circle = np.vstack([np.cos(linspace / n * 2 * np.pi), np.sin(linspace / n * 2 * np.pi)]).T
 data = [
-    circle + np.array([-1.1,-1.1]),
-    circle + np.array([1.1,-1.1]),
-    circle + np.array([1.1,1.1]),
-    circle + np.array([-1.1,1.1]),
+    circle + np.array([-0.6,-0.6]),
+    circle + np.array([0.6,-0.6]),
+    circle + np.array([0.6,0.6]),
+    circle + np.array([-0.6,0.6]),
 ]
 
+#circle = np.vstack([np.cos(linspace / n * 2 * np.pi), np.sin(linspace / n * 2 * np.pi)]).T
+#data = [
+#    circle + np.array([-1.1,-1.1]),
+#    circle + np.array([1.1,-1.1]),
+#    circle + np.array([1.1,1.1]),
+#    circle + np.array([-1.1,1.1]),
+#]
 
-epsilon = 1.0
+#x1 = multivariate_normal.rvs(size=(12,2)) / 4
+#x2 = multivariate_normal.rvs(size=(12,2)) / 4
+#x3 = multivariate_normal.rvs(size=(12,2)) / 4
+#x4 = multivariate_normal.rvs(size=(12,2)) / 4
+
+#x1 += np.array([-1.5,-0.5])
+#x2 += np.array([-1.5,0.5])
+#x3 += np.array([1.5,-0.5])
+#x4 += np.array([1.5,0.5])
+
+#data = [ x1, x2, x3, x4 ]
+
+epsilon = 0.3
 
 print("Performing eMOT")
 r_ks = np.ones((m,n+1)) / (n) 
@@ -32,8 +52,8 @@ X, C = approximate_MOT(data, r_ks, epsilon, euclidean, 0.3)
 print("Total Cost", (X*C).sum())
 
 print("Performing fixed point")
-Y, Ymass, gammas = fixed_point(data, epsilon, euclidean, 
-                               npoints=100, num_iters=25, barrier=3)
+Y, Ymass, gammas, _ = incentive_fixed_point(data, epsilon, euclidean, 
+                               npoints=10, num_iters=25, barrier=3)
 print("Total Cost", np.sum(Ymass))
 print("   Violating Mass", violating_mass(data, Y, gammas, epsilon, euclidean))
 
